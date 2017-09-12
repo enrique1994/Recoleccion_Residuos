cda055adf6e953c27bf6d83b3a2a412d6571c7a0
Aimee <zvok59@gmail.com>
2017-09-11 22:19:49 -0500

    Métodos para inicio de sesión del empleado

--- app/src/main/java/recoleccion/de/planeacion/app/com/fepro2017final/LoginActivity2.java
+++ app/src/main/java/recoleccion/de/planeacion/app/com/fepro2017final/LoginActivity2.java
@@ -10,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -39,13 +40,29 @@ import android.view.MenuItem;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity2 extends AppCompatActivity implements OnNavigationItemSelectedListener{
-
+    private EditText id_empleado;
+    private EditText password_empleado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        id_empleado = (EditText)findViewById(R.id.id_empleado);
+        password_empleado = (EditText)findViewById(R.id.password_empleado);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -70,13 +87,104 @@ public class LoginActivity2 extends AppCompatActivity implements OnNavigationIte
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), MainActivity_Recolector.class);
-                startActivityForResult(intent, 0);
+                //Intent intent = new Intent (v.getContext(), MainActivity_Recolector.class);
+                //startActivityForResult(intent, 0);
+                login_empleado();
             }
         });
 
     }
 
+    public void login_empleado() {
+        final String id = id_empleado.getText().toString();
+        final String password = password_empleado.getText().toString();
+        //final String id = "55";
+        //final String password = "jaiem";
+        HashMap<String, String> map = new HashMap<>();// MAPEO
+
+        map.put("id", id);// Identificador
+        map.put("password", password);
+
+        JSONObject jobject = new JSONObject(map);// Objeto Json
+
+        // Comprobar si existe el usuario
+        VolleySingleton.getInstance(LoginActivity2.this).addToRequestQueue(
+                new JsonObjectRequest(
+                        Request.Method.POST,
+                        Constantes.LOGIN_EMPLEADO,
+                        jobject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // Procesar la respuesta
+                                procesarRespuestaLogin(response);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("login_empleado()1 ", "Error Volley: " + error.getMessage());
+                            }
+                        }
+
+                ) {
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        Map<String, String> headers = new HashMap<>();
+                        headers.put("Content-Type", "application/json; charset=utf-8");
+                        headers.put("Accept", "application/json");
+                        return headers;
+                    }
+
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8" + getParamsEncoding();
+                    }
+                }
+        );
+    }
+
+    /**
+     * Procesa la respuesta
+     */
+    private void procesarRespuestaLogin(JSONObject response) {
+
+        try {
+            // Obtener estado
+            String estado = response.getString("estado");
+            // Obtener mensaje
+            String mensaje = response.getString("mensaje");
+
+            switch (estado) {
+                case "1":
+                    // Mostrar mensaje
+                    Toast.makeText(
+                            LoginActivity2.this,
+                            mensaje,
+                            Toast.LENGTH_LONG).show();
+                    // Enviar código de éxito
+                    LoginActivity2.this.setResult(203);
+                    Intent intent = new Intent (LoginActivity2.this, MainActivity_Recolector.class);
+                    startActivityForResult(intent, 0);
+
+                    break;
+
+                case "2":
+                    // Mostrar mensaje
+                    Toast.makeText(
+                            LoginActivity2.this,
+                            mensaje,
+                            Toast.LENGTH_LONG).show();
+                    // Enviar código de falla
+                    LoginActivity2.this.setResult(Activity.RESULT_CANCELED);
+
+                    break;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
